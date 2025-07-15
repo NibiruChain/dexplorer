1a9d464d84486a225f04b444e34676d0a78b71eb
mmntk <mmntk@users.noreply.github.com>
2025-07-15 18:17:59 +0300

    feat: add env support for chain list

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    NEXT_CHAIN_NAME: process.env.NEXT_PUBLIC_CHAIN_NAME,
+    NEXT_CHAIN_RPC: process.env.NEXT_PUBLIC_CHAIN_RPC,
+  },  
 }
 
 module.exports = nextConfig
